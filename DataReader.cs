e89630a1c96f3a427061ff1b461547d95a112f7f
William  Adam-Grenier <william.a-g@hotmail.com>
2021-01-18 13:50:29 -0500

    Program Finds All Less Archives

---
+++ DataReader.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Zero2Unpacker
+{
+    public class DeLESSFile
+    {
+        public long startingPosition = 0;
+        public long endingPosition = 0;
+        public long fileSize = 0;
+    }
+
+    public class DataReader
+    {
+        private string fileName;
+        private string directory;
+        private long img_size;
+        private FileInfo fileInfo;
+        private BinaryReader dataStream;
+        public List<DeLESSFile> delessFiles = new List<DeLESSFile>();
+
+        public DataReader(string fileName, string directory)
+        {
+            this.fileName = fileName;
+            this.directory = directory;
+            this.dataStream = new BinaryReader(new FileStream($"{this.directory}\\{this.fileName}", FileMode.Open, FileAccess.Read));
+            this.fileInfo = new FileInfo($"{this.directory}\\{this.fileName}");
+            this.img_size = this.fileInfo.Length;
+        }
+
+        public void SplitDeLESSArchives()
+        {
+            var currFile = new DeLESSFile();
+            currFile.startingPosition = 0;
+
+            var pattern = new byte[] {0x4c, 0x45, 0x53, 0x53};
+
+            // Ships the initial DeLESS Header
+            this.dataStream.BaseStream.Position = 0x8;
+
+            var searchPosition = 0;
+
+            while (this.dataStream.BaseStream.Position < img_size) //Loop until we reach the end of the file
+            {
+                var latestbyte = this.dataStream.ReadByte();
+
+                if (latestbyte == -1)
+                {
+                    break; //We have reached the end of the file
+                }
+
+                if (latestbyte != pattern[searchPosition])
+                {
+                    searchPosition = 0;
+                    //break; //We have reached the end of the file
+                }
+
+                else if (latestbyte == pattern[searchPosition])
+                {
+                    searchPosition++;
+                    if (searchPosition == pattern.Length)
+                    {
+                        currFile.endingPosition = this.dataStream.BaseStream.Position - 0x5;
+                        delessFiles.Add(currFile);
+
+                        currFile = new DeLESSFile()
+                        {
+                            startingPosition = this.dataStream.BaseStream.Position - 0x4
+                        };
+
+                        searchPosition = 0;
+                    }
+                }
+            }
+
+            currFile.endingPosition = this.dataStream.BaseStream.Position;
+            delessFiles.Add(currFile);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.IO;
 
 namespace Zero2Unpacker
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             *
+             * 1 - Extract all DeLESS files into their own files
+             * 2 - Run DeLESS on each of these files
+             * 3 - Extract each files from said unarchived files
+             *
+             */
+
+            var dataReader = new DataReader("IMG_BD.BIN", ".");
+
+            dataReader.SplitDeLESSArchives();
+
+            Console.WriteLine($"Total files found : {dataReader.delessFiles.Count}");
+
+            foreach (var file in dataReader.delessFiles)
+            {
+                Console.WriteLine($"Starting pos: {file.startingPosition}, ending pos: {file.endingPosition}");
+            }
+        }
+
+        
+
+
+        public static void RunDeLESS()
+        {
+            var directoryName = "";
+            var destfolderName = "";
+            var destfileName = "";
+
+            var command = $" {directoryName}\\{destfolderName.Replace("..", "")}{destfileName}";
+            var path = Directory.GetCurrentDirectory();
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+
+            process.StartInfo.FileName = $"{path}\\DeLESS.exe";
+            process.StartInfo.WorkingDirectory = path;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.Arguments = command;
+            process.Start();
+            process.WaitForExit();
+
+            var dname = $"{directoryName}\\{destfolderName.Replace("..", "")}{destfileName}";
+            var temp = $"{directoryName}\\{destfolderName.Replace("..", "")}{destfileName}.LED";
+
+            File.Delete(dname);
+            File.Move(temp, dname);
         }
     }
 }
