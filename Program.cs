27e0dd3629f5d9a70a5bd44d963ebd0a628d7e33
William  Adam-Grenier <william.a-g@hotmail.com>
2021-02-01 13:24:47 -0500

    Removed Useless Info

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using CommandLine;
+﻿using CommandLine;
 
 namespace Zero2Unpacker
 {
@@ -8,12 +7,6 @@ namespace Zero2Unpacker
         public static void Main(string[] args)
         {
             /*
-             * Add this to make the extractor more enjoyable
-             * https://archive.codeplex.com/?p=commandline
-             *
-             * 1 - Extract all DeLESS files into their own files
-             * 2 - Run DeLESS on each of these files
-             * 3 - Extract each files from said unarchived files
              *
              * Memory location with RegisterFile: 0x003c7fdb
              *
